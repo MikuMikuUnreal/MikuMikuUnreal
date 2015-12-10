c8d5c323e2cd399d394e879d10461dfc5405c969
Andrzej Trzaska <atrzaska2@gmail.com>
2015-12-10 17:39:44 +0100

    Initial commit

---
+++ Source/MikuMikuUnreal/MikuMikuUnrealGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "MikuMikuUnrealGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MIKUMIKUUNREAL_API AMikuMikuUnrealGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
